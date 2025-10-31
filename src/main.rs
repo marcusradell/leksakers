a008ea5f374cd8755549abe3d6f53743b8bcb31c
Marcus Rådell <marcus@radell.net>
2025-10-31 22:55:29 +0100

    add panic semihosting

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@
 
 use cortex_m_rt::entry;
 use microbit as _;
-use panic_halt as _;
+// use panic_halt as _;
+use panic_semihosting as _;
 
 #[entry]
 fn main() -> ! {
+    panic!("test-panic");
     let _y;
     let x = 42;
     _y = x;
