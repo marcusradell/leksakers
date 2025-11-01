9265cfb4d72cffd5db140a3422247cb1b4178b79
Marcus Rådell <marcus@radell.net>
2025-11-01 12:31:45 +0100

    unwrap the board

--- src/main.rs
+++ src/main.rs
@@ -16,48 +16,46 @@ use microbit::{
 
 #[entry]
 fn main() -> ! {
-    if let Some(board) = Board::take() {
-        let mut timer = Timer::new(board.TIMER0);
-        let mut display = Display::new(board.display_pins);
+    let board = Board::take().unwrap();
 
-        // initialize adc
-        let saadc_config = SaadcConfig::default();
-        let mut saadc = Saadc::new(board.ADC, saadc_config);
-        let mut mic_in = board.microphone_pins.mic_in.into_floating_input();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
 
-        // enable microphone
-        board
-            .microphone_pins
-            .mic_run
-            .into_open_drain_output(OpenDrainConfig::Disconnect0HighDrive1, Level::High);
+    // initialize adc
+    let saadc_config = SaadcConfig::default();
+    let mut saadc = Saadc::new(board.ADC, saadc_config);
+    let mut mic_in = board.microphone_pins.mic_in.into_floating_input();
 
-        let mut count: u64 = 0;
-        let mut sum: u64 = 0;
-        let mut max_value: u16 = 0;
-        loop {
-            let mic_value = saadc
-                .read_channel(&mut mic_in)
-                .expect("could not read value of microphone") as u16;
+    // enable microphone
+    board
+        .microphone_pins
+        .mic_run
+        .into_open_drain_output(OpenDrainConfig::Disconnect0HighDrive1, Level::High);
 
-            // Smoothen the signal as audio comes in waves
-            max_value = max_value.max(mic_value);
-            sum += mic_value as u64;
-            count += 1;
+    let mut count: u64 = 0;
+    let mut sum: u64 = 0;
+    let mut max_value: u16 = 0;
+    loop {
+        let mic_value = saadc
+            .read_channel(&mut mic_in)
+            .expect("could not read value of microphone") as u16;
 
-            if count % 100 == 0 {
-                let avg = (sum / count) as u16;
-                let image = [
-                    [if max_value > avg + 100 / 2 { 1 } else { 0 }; 5],
-                    [if max_value > avg + 80 / 2 { 1 } else { 0 }; 5],
-                    [if max_value > avg + 60 / 2 { 1 } else { 0 }; 5],
-                    [if max_value > avg + 40 / 2 { 1 } else { 0 }; 5],
-                    [if max_value > avg + 20 / 2 { 1 } else { 0 }; 5],
-                ];
-                display.show(&mut timer, image, 100);
-                max_value = 0;
-            }
+        // Smoothen the signal as audio comes in waves
+        max_value = max_value.max(mic_value);
+        sum += mic_value as u64;
+        count += 1;
+
+        if count % 100 == 0 {
+            let avg = (sum / count) as u16;
+            let image = [
+                [if max_value > avg + 100 / 2 { 1 } else { 0 }; 5],
+                [if max_value > avg + 80 / 2 { 1 } else { 0 }; 5],
+                [if max_value > avg + 60 / 2 { 1 } else { 0 }; 5],
+                [if max_value > avg + 40 / 2 { 1 } else { 0 }; 5],
+                [if max_value > avg + 20 / 2 { 1 } else { 0 }; 5],
+            ];
+            display.show(&mut timer, image, 100);
+            max_value = 0;
         }
     }
-
-    panic!("End");
 }
